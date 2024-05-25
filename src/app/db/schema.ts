33747e5450206c5fd3f001ef4b798fd2347e7af8
Vadim Firulyov <firulvv@mail.ru>
2024-05-25 20:08:49 +0400

    add transaction

--- src/app/db/schema.ts
+++ src/app/db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { text, pgTable } from "drizzle-orm/pg-core";
+import { text, pgTable, integer, timestamp } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -16,7 +16,11 @@ export const userRelations = relations(user, ({ many }) => ({
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
-  userId: text("user_id").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, {
+      onDelete: "cascade",
+    }),
 });
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -28,14 +32,58 @@ export const accountsRelations = relations(accounts, ({ one }) => ({
 
 export const categories = pgTable("categories", {
   id: text("id").primaryKey(),
-  categoryId: text("category_id"),
   name: text("name").notNull(),
-  userId: text("user_id").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, {
+      onDelete: "cascade",
+    }),
 });
 
 export const categoriesRelations = relations(categories, ({ one }) => ({
   user: one(user, {
     fields: [categories.userId],
     references: [user.id],
+  }),
+}));
+
+export const transactions = pgTable("transactions", {
+  id: text("id").primaryKey(),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, {
+      onDelete: "cascade",
+    }),
+
+  accountId: text("account_id")
+    .references(() => accounts.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  user: one(user, {
+    fields: [transactions.userId],
+    references: [user.id],
+  }),
+
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
   }),
 }));
